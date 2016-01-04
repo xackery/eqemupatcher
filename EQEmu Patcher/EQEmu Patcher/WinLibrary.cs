8a5ae951825156b559688ee0d1d88089832510f0
Xackery <xackery@gmail.com>
2016-01-04 15:57:07 -0800

    Added WinLibrary, used for registry interaction

---
+++ EQEmu Patcher/EQEmu Patcher/WinLibrary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace EQEmu_Patcher
+{
+    /* Windows Based Library Calls */
+    class WinLibrary
+    {
+        //KeyName: HKEY_CURRENT_USER\\RegistrySetValueExample
+        public static object GetRegistryValue(string keyName, string valueName, object defaultValue)
+        {
+            return Registry.GetValue(keyName, valueName, defaultValue);
+        }
+
+        //KeyName: HKEY_CURRENT_USER\\RegistrySetValueExample
+        public static void SetRegistryValue(string keyName, string valueName, object value)
+        {
+            Registry.SetValue(keyName, valueName, value);
+        }
+    }
+}
