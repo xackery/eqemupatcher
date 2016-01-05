8c718f6ea54654764166cbe06ca87197532c3eff
Xackery <xackery@gmail.com>
2016-01-04 16:10:00 -0800

    added a simple MD5 Creator

---
+++ EQEmu Patcher/EQEmu Patcher/UtilityLibrary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace EQEmu_Patcher
+{
+    /* General Utility Methods */
+    class UtilityLibrary
+    {
+        public static string GetMD5(string filePath)
+        {
+            var md5 = MD5.Create();
+            var stream = File.OpenRead(filePath);
+            var bytes =  md5.ComputeHash(stream);
+            return bytes.ToString();
+        }
+    }
+}
