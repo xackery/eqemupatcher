4cf8ebe98bcee536044e3ce78190065807eb26c0
Xackery <xackery@gmail.com>
2016-01-05 00:24:16 -0800

    added GetJson and IsEverquestDirectory

--- EQEmu Patcher/EQEmu Patcher/UtilityLibrary.cs
+++ EQEmu Patcher/EQEmu Patcher/UtilityLibrary.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 using System.IO;
+using System.Net;
 
 namespace EQEmu_Patcher
 {
@@ -17,6 +18,33 @@ namespace EQEmu_Patcher
             var stream = File.OpenRead(filePath);
             var bytes =  md5.ComputeHash(stream);
             return bytes.ToString();
+        }
+
+        public static string GetJson(string urlPath)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                return wc.DownloadString(urlPath);
+            }
+        }
+
+        public static bool IsEverquestDirectory()
+        {
+
+            var di = new System.IO.DirectoryInfo(System.Reflection.Assembly.GetEntryAssembly().Location);
+            //try {
+            var files = di.GetFiles("eqgame.exe");
+            //}
+            //catch (UnauthorizedAccessException e)
+            //{
+
+            // This code just writes out the message and continues to recurse.
+            // You may decide to do something different here. For example, you
+            // can try to elevate your privileges and access the file again.
+            //    log.Add(e.Message);
+            // }
+
+            return files != null;
         }
     }
 }
