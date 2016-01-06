d53ed985d9dd29180e60037e1173de8cde7b9710
Xackery <xackery@gmail.com>
2016-01-06 01:02:30 -0800

    Fixed anchoring so patcher is resizable

--- EQEmu Patcher/EQEmu Patcher/MainForm.Designer.cs
+++ EQEmu Patcher/EQEmu Patcher/MainForm.Designer.cs
@@ -31,17 +31,23 @@
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.txtList = new System.Windows.Forms.TextBox();
             this.btnScan = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // progressBar
             // 
-            this.progressBar.Location = new System.Drawing.Point(12, 298);
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.Location = new System.Drawing.Point(93, 299);
             this.progressBar.Name = "progressBar";
-            this.progressBar.Size = new System.Drawing.Size(359, 23);
+            this.progressBar.Size = new System.Drawing.Size(278, 23);
             this.progressBar.TabIndex = 0;
             // 
             // txtList
             // 
+            this.txtList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.txtList.Location = new System.Drawing.Point(13, 13);
             this.txtList.Multiline = true;
             this.txtList.Name = "txtList";
@@ -50,7 +56,8 @@
             // 
             // btnScan
             // 
-            this.btnScan.Location = new System.Drawing.Point(377, 298);
+            this.btnScan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnScan.Location = new System.Drawing.Point(12, 298);
             this.btnScan.Name = "btnScan";
             this.btnScan.Size = new System.Drawing.Size(75, 24);
             this.btnScan.TabIndex = 2;
@@ -58,11 +65,23 @@
             this.btnScan.UseVisualStyleBackColor = true;
             this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
             // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(377, 299);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 24);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Play";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(464, 333);
+            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.btnScan);
             this.Controls.Add(this.txtList);
             this.Controls.Add(this.progressBar);
@@ -79,6 +98,7 @@
         private System.Windows.Forms.ProgressBar progressBar;
         private System.Windows.Forms.TextBox txtList;
         private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Button btnStart;
     }
 }
 
