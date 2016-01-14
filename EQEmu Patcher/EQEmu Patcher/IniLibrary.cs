ca1fcb774f71486d07176ce3c519bd20a1366edc
Xackery <xackery@gmail.com>
2016-01-14 02:06:21 -0800

    Added IniLibrary, a tool for loading and saving eqemupatcher.ini options (defined inside it's class).

---
+++ EQEmu Patcher/EQEmu Patcher/IniLibrary.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQEmu_Patcher
+{
+    class IniLibrary
+    {
+        public static IniLibrary instance;
+        public bool IsCleanCopy;
+        public VersionTypes ClientVersion;
+
+        
+        public static void Save()
+        {
+            File.WriteAllText(@"eqemupatcher.ini", JsonConvert.SerializeObject(instance));
+        }
+
+        public static void Load()
+        {
+            try {
+                instance = Newtonsoft.Json.JsonConvert.DeserializeObject<IniLibrary>(File.ReadAllText(@"eqemupatcher.ini"));
+                if (instance == null) {
+                    ResetDefaults();
+                }
+            } catch (System.IO.FileNotFoundException) {                
+                ResetDefaults();
+                Save();
+            }
+        }
+
+        public static void ResetDefaults()
+        {
+            instance = new IniLibrary();
+        }
+    }
+}
