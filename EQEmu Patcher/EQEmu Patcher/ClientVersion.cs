14dadc9962a19ab2667591681dfa785fa4436f51
Xackery <xackery@gmail.com>
2016-01-14 02:05:47 -0800

    Added ClientVersion, a class that stores every known client type

---
+++ EQEmu Patcher/EQEmu Patcher/ClientVersion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQEmu_Patcher
+{    
+    public enum VersionTypes
+    {
+        Unknown,
+        Titanium,
+        Rain_Of_Fear,
+        Rain_Of_Fear_2,
+        Seeds_Of_Destruction,
+        Underfoot,
+        Secrets_Of_Feydwer
+    }
+
+    class ClientVersion
+    {
+        public string FullName;
+        public string ShortName;
+
+        public ClientVersion(string fName, string sName)
+        {
+            FullName = fName;
+            ShortName = sName;
+        }
+    }
+}
