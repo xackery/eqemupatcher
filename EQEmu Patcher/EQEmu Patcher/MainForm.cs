6b5b65f5d86b2ecf4b184158575a7c06e8c2c035
Xackery <xackery@gmail.com>
2016-01-14 02:04:58 -0800

    revised client detection flow, to better respect IniLibrary

--- EQEmu Patcher/EQEmu Patcher/MainForm.cs
+++ EQEmu Patcher/EQEmu Patcher/MainForm.cs
@@ -12,79 +12,35 @@ using System.Windows.Forms;
 
 namespace EQEmu_Patcher
 {
+    
     public partial class MainForm : Form
     {
 
-        string clientVersion;
+        private Dictionary<VersionTypes, ClientVersion> clientVersions = new Dictionary<VersionTypes, ClientVersion>();
+
+        VersionTypes currentVersion;
 
        // TaskbarItemInfo tii = new TaskbarItemInfo();
         public MainForm()
         {
             InitializeComponent();
-        }
+        }           
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            //MessageBox.Show("In case you didn't realize it, this patcher doesn't work yet. At this time it's purely under development for testing.");
-           // tii.ProgressState = TaskbarItemProgressState.Normal;
-          //  tii.ProgressValue = (double)50 /100;
-            
-            //Read:
-            /*
-            var pv = JsonConvert.DeserializeObject<PatchVersion>(test);
-            MessageBox.Show(pv.ServerName);
-            */
-            //Write:
-            /*
-            PatchVersion pv = new PatchVersion();
-            pv.ServerName = "Test";
-            pv.RootFiles.Add("eqgame.exe", "12345test");
-            txtList.Text = JsonConvert.SerializeObject(pv);
-            */
-            try {
+            buildClientVersions();
+            IniLibrary.Load();
 
-                var hash = UtilityLibrary.GetEverquestExecutableHash(AppDomain.CurrentDomain.BaseDirectory);
-                if (hash == "") { 
-                    MessageBox.Show("Please run this patcher in your Everquest directory.");
-                    this.Close();
-                    return;
-                }
-                switch (hash)
-                {
-                    case "85218FC053D8B367F2B704BAC5E30ACC":
-                        txtList.Text = "You seem to have put me in a Secrets of Feydwer client directory";
-                        clientVersion = "Secrets of Feydwer";
-                        break;
-                    case "859E89987AA636D36B1007F11C2CD6E0":
-                        txtList.Text = "You seem to have put me in a Underfoot client directory";
-                        clientVersion = "Underfoot";
-                        break;
-                    case "BB42BC3870F59B6424A56FED3289C6D4":
-                        txtList.Text = "You seem to have put me in a Titanium client directory";
-                        clientVersion = "Titanium";
-                        break;
-                    case "368BB9F425C8A55030A63E606D184445":
-                        txtList.Text = "You seem to have put me in a Reign of Fear client directory";
-                        clientVersion = "Rain of Fear";
-                        break;
-                    case "240C80800112ADA825C146D7349CE85B":
-                        txtList.Text = "You seem to have put me in a Reign of Fear client directory";
-                        clientVersion = "Rain of Fear 2";
-                        break;
-                    default:
-                        if (MessageBox.Show("I don't recognize the EQ client in this directory, open a web page to let the devs know?", "Visit", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
-                        {
-                            System.Diagnostics.Process.Start("https://github.com/Xackery/eqemupatcher/issues/new?title=A+New+EQClient+Found&body=Hi+I+Found+A+New+Client!+Hash:+"+hash);
-                        }
-                        txtList.Text = "I don't recognize the Everquest client in my directory, send this to Shin: " + hash;
-                        break;
-                }
-                txtList.Text += "\r\n\r\nIf you wish to help out, press the scan button on the bottom left and wait for it to complete, then copy paste this data as an Issue on github!";
-            }
-            catch (UnauthorizedAccessException err)
+            if (IniLibrary.instance.ClientVersion == VersionTypes.Unknown)
             {
-                MessageBox.Show("You need to run this program with Administrative Privileges" + err.Message);
-                return;
+                detectClientVersion();
+                if (currentVersion == VersionTypes.Unknown)
+                {
+                    this.Close();
+                }
+                detectCleanCopy();
+                IniLibrary.instance.ClientVersion = currentVersion;
+                IniLibrary.Save();
             }
             
            
@@ -160,6 +116,86 @@ namespace EQEmu_Patcher
             return fileMap;
         }
 
+        private void detectCleanCopy()
+        {
+            //I use eqspells to detect clean copies, since it's the most common file to be edited.
+            if (MessageBox.Show("This directory appears to be a clean copy of " + clientVersions[currentVersion].FullName + ". Should we set this as the default?", "Clean Copy?", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
+            {
+                IniLibrary.instance.IsCleanCopy = true;
+            }
+        }
+
+        private void detectClientVersion()
+        {
+
+            try
+            {
+
+                var hash = UtilityLibrary.GetEverquestExecutableHash(AppDomain.CurrentDomain.BaseDirectory);
+                if (hash == "")
+                {
+                    MessageBox.Show("Please run this patcher in your Everquest directory.");
+                    this.Close();
+                    return;
+                }
+                switch (hash)
+                {
+                    case "85218FC053D8B367F2B704BAC5E30ACC":
+                        currentVersion = VersionTypes.Secrets_Of_Feydwer;
+                        break;
+                    case "859E89987AA636D36B1007F11C2CD6E0":
+                        currentVersion = VersionTypes.Underfoot;
+                        break;
+                    case "BB42BC3870F59B6424A56FED3289C6D4":
+                        currentVersion = VersionTypes.Titanium;
+                        break;
+                    case "368BB9F425C8A55030A63E606D184445":
+                        currentVersion = VersionTypes.Rain_Of_Fear;
+                        break;
+                    case "240C80800112ADA825C146D7349CE85B":
+                        currentVersion = VersionTypes.Rain_Of_Fear_2;
+                        break;
+                    default:
+                        currentVersion = VersionTypes.Unknown;
+                        break;
+
+                }
+                if (currentVersion == VersionTypes.Unknown)
+                {
+                    if (MessageBox.Show("Unable to recognize the Everquest client in this directory, open a web page to report to devs?", "Visit", MessageBoxButtons.YesNo, MessageBoxIcon.Asterisk) == DialogResult.Yes)
+                    {
+                        System.Diagnostics.Process.Start("https://github.com/Xackery/eqemupatcher/issues/new?title=A+New+EQClient+Found&body=Hi+I+Found+A+New+Client!+Hash:+" + hash);
+                    }
+                    txtList.Text = "Unable to recognize the Everquest client in this directory, send to developers: " + hash;
+                }
+                else
+                {
+                    txtList.Text = "You seem to have put me in a " + clientVersions[currentVersion].FullName + " client directory";
+                }
+
+
+                txtList.Text += "\r\n\r\nIf you wish to help out, press the scan button on the bottom left and wait for it to complete, then copy paste this data as an Issue on github!";
+            }
+            catch (UnauthorizedAccessException err)
+            {
+                MessageBox.Show("You need to run this program with Administrative Privileges" + err.Message);
+                return;
+            }
+        }
+
+        //Build out all client version's dictionary
+        private void buildClientVersions()
+        {
+            clientVersions.Clear();
+            clientVersions.Add(VersionTypes.Titanium, new ClientVersion("Titanium", "titanium"));
+            clientVersions.Add(VersionTypes.Secrets_Of_Feydwer, new ClientVersion("Secrets Of Feydwer", "sof"));
+            clientVersions.Add(VersionTypes.Seeds_Of_Destruction, new ClientVersion("Seeds of Destruction", "sod"));
+            clientVersions.Add(VersionTypes.Rain_Of_Fear, new ClientVersion("Rain of Fear", "rof"));
+            clientVersions.Add(VersionTypes.Rain_Of_Fear_2, new ClientVersion("Rain of Fear 2", "rof2"));
+            clientVersions.Add(VersionTypes.Underfoot, new ClientVersion("Underfoot", "underfoot"));
+
+        }
+
         private int getFileCount(System.IO.DirectoryInfo root) {
             int count = 0;
                            
@@ -201,7 +237,7 @@ namespace EQEmu_Patcher
           //  progressBar.Maximum += getFileCount(new System.IO.DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "\\atlas"));
             txtList.Text = "Max:" + progressBar.Maximum;
             PatchVersion pv = new PatchVersion();
-            pv.ClientVersion = clientVersion;
+            pv.ClientVersion = clientVersions[currentVersion].ShortName;
             //Root
             var fileMap = WalkDirectoryTree(new System.IO.DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory));
             pv.RootFiles = fileMap;
@@ -245,6 +281,10 @@ namespace EQEmu_Patcher
         {
             try {
                 process = UtilityLibrary.StartEverquest();
+                if (process != null)
+                {
+                    this.Close();
+                }
             }
             catch  (Exception err) {
                 MessageBox.Show("An error occured: "   + err.Message);
