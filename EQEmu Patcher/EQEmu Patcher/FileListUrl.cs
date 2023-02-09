1f4a75fd6b5ca34fe330330cdce1ec04e9a7bfd0
Xackery <xackery@gmail.com>
2023-02-09 12:48:52 -0800

    Add FileListUrl and ServerName assembly

---
+++ EQEmu Patcher/EQEmu Patcher/FileListUrl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EQEmu_Patcher
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class FileListUrl : Attribute
+    {
+        public string Value { get; set; }
+        public FileListUrl(string value)
+        {
+            Value = value;
+        }
+    }
+}
--- EQEmu Patcher/build.bat
+++ EQEmu Patcher/build.bat
@@ -1,5 +1,9 @@
-@echo off
 echo building....
-"C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\msbuild.exe" /property:Configuration=Release;Version=1.0.17.0
-copy "EQEmu patcher"\bin\Release\eqemupatcher.exe .
+set VERSION=1.0.17.0
+set BUILD_CONFIGURATION=Release
+set SERVER_NAME=EQEMU Patcher
+set FILELIST_URL=https://test.com
+
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\msbuild.exe" /m /p:Configuration=%BUILD_CONFIGURATION% /p:Version=%VERSION% /p:SERVER_NAME="%SERVER_NAME%" /p:FILELIST_URL="%FILELIST_URL%"
+copy "EQEmu Patcher"\bin\Release\eqemupatcher.exe .
 
