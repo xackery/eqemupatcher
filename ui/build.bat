d48378cf550cbc5ef8a6755b8062895d7f9c9aa3
Xackery <xackery@gmail.com>
2023-02-09 09:40:27 -0800

    Removed ui folder (golang)

--- ui/build.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rsrc -manifest eqemupatcher.manifest -o rsrc.syso
-taskkill /IM eqemupatcher.exe
-rm eqemupatcher.exe
-go build -ldflags="-H windowsgui" -o eqemupatcher.exe
-eqemupatcher.exe
