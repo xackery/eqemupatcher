4b5c6fa6177f3ecdeee0b07a94fa254dcd644893
Xackery <xackery@gmail.com>
2023-02-11 23:48:18 -0800

    Bump msbuild and misc fixes

--- EQEmu Patcher/build.bat
+++ EQEmu Patcher/build.bat
@@ -2,6 +2,6 @@ echo building....
 set VERSION=0.0.0.1
 set BUILD_CONFIGURATION=Release
 set SERVER_NAME=EQEMU Patcher
-set FILELIST_URL=https://github.com/xackery/eqemupatcher/releases/download/latest
-set PATCHER_URL=https://github.com/xackery/eqemupatcher/releases/download/latest
+set FILELIST_URL=https://github.com/xackery/eqemupatcher/releases/latest/download
+set PATCHER_URL=https://github.com/xackery/eqemupatcher/releases/latest/download/eqemupatcher.exe
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\msbuild.exe" /m /p:Configuration=%BUILD_CONFIGURATION% /p:VERSION=%VERSION% /p:SERVER_NAME="%SERVER_NAME%" /p:FILELIST_URL="%FILELIST_URL%" /p:PATCHER_URL="%PATCHER_URL%"
