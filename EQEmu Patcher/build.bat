ef7a08e0e848bde9f75f3fc009e74ea2c0a22059
Xackery <xackery@gmail.com>
2023-02-12 03:24:38 -0800

    Fix patcher url

--- EQEmu Patcher/build.bat
+++ EQEmu Patcher/build.bat
@@ -4,5 +4,5 @@ set BUILD_CONFIGURATION=Release
 set SERVER_NAME=EQEMU Patcher
 set FILE_NAME=emupatcher
 set FILELIST_URL=https://github.com/xackery/eqemupatcher/releases/latest/download
-set PATCHER_URL=https://github.com/xackery/eqemupatcher/releases/latest/download/eqemupatcher-hash.txt
+set PATCHER_URL=https://github.com/xackery/eqemupatcher/releases/latest/download
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\msbuild.exe" /m /p:Configuration=%BUILD_CONFIGURATION% /p:VERSION=%VERSION% /p:SERVER_NAME="%SERVER_NAME%" /p:FILELIST_URL="%FILELIST_URL%" /p:PATCHER_URL="%PATCHER_URL%" /p:FILE_NAME="%FILE_NAME%"
